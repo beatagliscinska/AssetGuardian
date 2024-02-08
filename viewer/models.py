946bec8524945b50d44530a6fb3d8464533cbe28
beatagliscinska <beata.gliscinska@gmail.com>
2024-02-08 20:43:55 +0100

    Added migrations files and tempalates

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+from django.db.models import (
+ DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
+ Model, TextField, DecimalField, CASCADE
+)
 
-# Create your models here.
+
+class AssetCategory(Model):
+    objects = None
+    name = CharField(max_length=128)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Employees(Model):
+    objects = None
+    name = CharField(max_length=128)
+    surname = CharField(max_length=128)
+    position = CharField(max_length=128)
+    department = CharField(max_length=128)
+    manager = CharField(max_length=128)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Asset(Model):
+    objects = None
+    asset_number = CharField(max_length=128, default=True)
+    category = ForeignKey(AssetCategory, on_delete=DO_NOTHING)
+    description = TextField()
+    vendor = CharField(max_length=128)
+    serial_number = IntegerField(default=True)
+    value = DecimalField(max_digits=10, decimal_places=2)
+    assigned_to = ForeignKey(Employees, on_delete=DO_NOTHING)
+    purchase_date = DateField()
+
+    def __str__(self):
+        return str(self.description)
--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# from django.views import View
+# from django.views.generic import TemplateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from logging import getLogger
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from viewer.models import Asset
+
+
+def hello(request, s0):
+    s1 = request.GET.get('s1', '')
+    return render(
+        request, template_name='hello.html',
+        context={'adjectives': [s0, s1, 'money', 'deadlines']}
+    )
+
+
+class AssetsView(ListView):
+    template_name = 'assets.html'
+    model = Asset
