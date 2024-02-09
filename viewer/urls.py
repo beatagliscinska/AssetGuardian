14852f83ab376d80661feaf2f70135e5f22555e9
beatagliscinska <beata.gliscinska@gmail.com>
2024-02-09 20:06:55 +0100

    Added home functionality

--- viewer/urls.py
+++ viewer/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from viewer.views import AssetView
+from viewer.views import AssetView, TemplateView
 
 urlpatterns = [
-       path('assets/', AssetView.as_view(), name='assets')
+       path('assets/', AssetView.as_view(), name='assets'),
+       path('', TemplateView.as_view(template_name='home.html'), name='home')
 ]
 
